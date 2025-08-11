706a66e63722fa72482d45777635ba11b99ce34a
Anetta <anettakawasaki@gmail.com>
2025-08-11 15:30:16 +0900

    save image card

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,8 +6,10 @@ import Search from "./components/Search";
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 const App = () => {
-  const [word, setWord] = useState('');
+  const [word, setWord] = useState("");
   const [images, setImages] = useState([]);
+
+  console.log(images);
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
@@ -22,7 +24,7 @@ const App = () => {
       .catch((err) => {
         console.log(err);
       });
-    setWord('');
+    setWord("");
   };
 
   return (
